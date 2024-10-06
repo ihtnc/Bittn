f00f519870c7ff7baf1a3d32f6176b31571b8440
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2024-10-06 14:37:50 +0300

    use correct framework

--- scripts/run-app.cmd
+++ scripts/run-app.cmd
@@ -1,3 +1,3 @@
 @echo off
-xcopy ..\app\mocks ..\app\src\bin\Debug\netcoreapp8.0\mocks /Y /I /E
+xcopy ..\app\mocks ..\app\src\bin\Debug\net8.0\mocks /Y /I /E
 dotnet run --project ..\app\src\Bittn.Api.csproj
